cef976758eea8111790ee7e275f8e1b14b0afeb8
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2025-08-31 20:48:58 +0100

    Update tasks27_8_2025.py

--- tasks27_8_2025.py
+++ tasks27_8_2025.py
@@ -102,28 +102,34 @@
  
 
 # #task7  
-#not working
-# counter = 0 
+
 # mean = 0
 # mode = 0
 # data = [5,3,8,3,9,12,5,3] 
+# mean = sum(data) / len(data)
+# count = data[0]
+# most_count = 0
+# for i in range(len(data)):
+#     frequ = 0
+#     for y in range(len(data)):
+#         if data[y] == data[y]:
+#             frequ += 1
+#         if frequ > most_count:
+#             most_count = frequ
+#             mode = data[i]
 # 
-# for i in range(0,len(data)):
-#     mean =mean +data[i]
-#     counter=data.count[i] 
-# 
-# if counter >0: 
-#     i = mode 
+# data.sort()
 # 
 # print("the mean", mean) 
 # 
 # print("the mode", mode) 
+# print("smallest", data[0])
+# print("largest", data[-1])
+#  
 
  
 
- 
-# 
-# #task8 
+#task8 
 
 # list = [] 
 # 
@@ -136,45 +142,66 @@
 # for i in range(0,5):
 #     if list[i]%2==0:
 #         print(list[i]) 
-
- 
-
+# 
+#  
+# 
 # #task 9 
+
+
+##def get_range(nums): 
 # 
-# def get_range(range): 
+#     return max(nums) - min(nums) 
 # 
-# 	values.sort() 
+# nums = [4,7,1,9,3]
+# print("range", get_range(nums))
 # 
-# 	range = (values[-1]-values[0]) 
 # 
-# 	print("the range is" , range) 
+
 # 
-# values= []
+# task10
+
+# 
+def get_range(values): 
+
+    return max(values) - min(values) 
+
+def get_mean(values):
+     return sum(values) / len(values)
+    
+def get_median(values):
+    values = sorted(values)
+    n = len(values)
+    mid = n// 2
+    if n % 2 == 0:
+        return (values[mid-1] +values[mid]) / 2
+    else:
+        return values[mid]
+
+
 #
-def get_range(values): 
+def get_mode(values):
+    count = values[0]
+    most_count = 0     
+    mode = 0
+    for i in range(len(values)):
+        frequ = 0
+        for y in range(len(values)):
+            if values[i] == values[y]:
+                frequ += 1
+                
+        if frequ > most_count:
+            most_count = frequ
+            mode = values[i]
+    return mode     
 
-    return max(values) - min(values) 
 
-nums = [4,7,1,9,3]
-print("range", get_range(nums))
+def get_max(values):
+    return max(values)
 
-
-
-
-
-import statistics 
-
- 
-
-def get_range(values): 
-
-    return max(values) - min(values) 
-
- 
-
+def get_min(values):
+    return min(values)
 
 numbers = [] 
-
 for i in range(6): 
 
     num = float(input("Enter number: ")) 
@@ -183,33 +210,23 @@ for i in range(6):
 
  
 
-# Calculate values 
 
-mean = statistics.mean(numbers) 
 
-median = statistics.median(numbers) 
 
-mode = statistics.mode(numbers) 
-
-maximum = max(numbers) 
-
-minimum = min(numbers) 
 
 range_val = get_range(numbers) 
 
 
-print("Data Analysis Results") 
-
 print("Numbers:", numbers) 
 
-print("Mean:", mean) 
+print("Mean:", get_mean(numbers)) 
 
-print("Median:", median) 
+print("Median:", get_median(numbers)) 
 
-print("Mode:", mode) 
+print("Mode:", get_mode(numbers)) 
 
-print("Maximum:", maximum) 
+print("Maximum:", get_max(numbers)) 
 
-print("Minimum:", minimum) 
+print("Minimum:", get_min(numbers)) 
 
 print("Range:", range_val)  
